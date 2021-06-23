d100600ba0543d369167218758849145a0b3bfbb
unknown <deveshpatel1@outlook.com>
2021-06-23 19:03:13 -0400

    added delete to apiRoutes

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -13,3 +13,10 @@ route.post('/notes', (req, res) => {
     .then((note) => res.json(note) )
     .catch((err) => res.status(500).json(err));
 });
+router.delete('/notes/:id', (req, res) => {
+    store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true}))
+    .catch((err) => res.status(500).json(err));
+});
+module.exports = router;
