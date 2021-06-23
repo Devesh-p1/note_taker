f23845117293c4393abb56da6b14f4bb9554eb1e
unknown <deveshpatel1@outlook.com>
2021-06-23 18:22:40 -0400

    added read/write to store.js

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -3,3 +3,10 @@ const fs = require('fs');
 const uid = require('uuidv1');
 const readFA = util.promisify(fs.readFile);
 const writeFA = util.promisify(fs.writeFile);
+class Store {
+    read() {
+      return readFA('db/db.json', 'utf8');
+    }
+    write(note) {
+      return writeFA('db/db.json', JSON.stringify(note));
+    }
